87e2321690ec0bb7cd4b44fab03fd1ccb52a511c
ICE0208 <doky3307@naver.com>
2022-05-13 15:11:44 +0900

    시스템 속성 추가 (테마 자동 적용)

--- src/main/MyFrame.java
+++ src/main/MyFrame.java
@@ -51,6 +51,7 @@ public class MyFrame extends JFrame{
 	
 	public static void main(String[] args) {
 //		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		System.setProperty("apple.awt.application.appearance", "system"); // Apple Appearance (able to be Dark Mode)
 		MyFrame mf = new MyFrame();
 		
 		
