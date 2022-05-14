cd192890aadd3fe349c513dbca32b68dbd1e34e9
ICE0208 <doky3307@naver.com>
2022-05-14 19:00:57 +0900

    그려지기 전과 후의 선 순서 차이 버그 해결

--- src/mypanel/PaintPanel.java
+++ src/mypanel/PaintPanel.java
@@ -27,27 +27,8 @@ public class PaintPanel extends JPanel {
 		 super.paint(g);
 		 	
 		 Graphics2D g2 = (Graphics2D) g;
-		 g2.setColor(Color.BLACK);
-		 g2.setStroke(new BasicStroke(5));
 		 
-		 ArrayList<PointInfo> paintingPoints = PaintListener.tempPoints;
-		 if (paintingPoints != null && paintingPoints.size() > 0) {
-			 PointInfo startP;
-			 PointInfo endP;
-			 
-			 startP = paintingPoints.get(0);
-			 g2.setColor(startP.color);
-			 g2.setStroke(startP.stroke);
-			 for(int j=0; j<paintingPoints.size(); j++) {
-				 endP = paintingPoints.get(j);
-				 g2.draw(new Line2D.Float(startP.x, startP.y, endP.x, endP.y));
-				 startP = endP;
-			 }
-			 
-		 }
-		 
-		 
-		 if (MyFrame.points.size() < 1) return;
+		 // OldPointsArray
 		 for (int i=0; i<MyFrame.points.size(); i++) {
 			 PointInfo startP;
 			 PointInfo endP;
@@ -60,6 +41,22 @@ public class PaintPanel extends JPanel {
 			 for(int j=0; j<pointObject.size(); j++) {
 				 endP = pointObject.get(j);
 //				 g.drawLine(startP.x, startP.y, endP.x, endP.y);
+				 g2.draw(new Line2D.Float(startP.x, startP.y, endP.x, endP.y));
+				 startP = endP;
+			 }
+		 }
+		 
+		 // PaintingPoints
+		 ArrayList<PointInfo> paintingPoints = PaintListener.tempPoints;
+		 if (paintingPoints != null && paintingPoints.size() > 0) {
+			 PointInfo startP;
+			 PointInfo endP;
+			 
+			 startP = paintingPoints.get(0);
+			 g2.setColor(startP.color);
+			 g2.setStroke(startP.stroke);
+			 for(int j=0; j<paintingPoints.size(); j++) {
+				 endP = paintingPoints.get(j);
 				 g2.draw(new Line2D.Float(startP.x, startP.y, endP.x, endP.y));
 				 startP = endP;
 			 }
